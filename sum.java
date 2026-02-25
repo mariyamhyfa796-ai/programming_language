c995e382b029363af2e723547d5687ee910e7fc5
hyfa <mariyamhyfa786@gmail.com>
2026-02-25 10:05:02 +0530

    hi

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+class sum
+  {
+    public static void main (string args[])
+     {
+         Scanner S= new Scanner(system.in);
+         System.out.println("enter a first number");
+         int a=S.nextInt();
+         System.out.println("enter a second number");
+         int b=S.nextInt();
+         int sum=a+b;
+          System.out.println("the sum of two number is"+sum);
+      }
+  }
---
+++ sum.py
@@ -0,0 +1,4 @@
+a=(input("enter a first number"))
+b=(input("enter a second number"))
+sum=a+b
+print("the sum of two number is",sum)
